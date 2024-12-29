60ebe5e2f15d2528bee61f65b8979173b84bafc9
Steven Pousty <steve.pousty@gmail.com>
2024-12-29 01:00:21 +0000

    Moving right along

--- 3_viz_image_patterns.py
+++ 3_viz_image_patterns.py
@@ -19,20 +19,31 @@ dataset = fo.Dataset.from_dir(
     dataset_dir=PHOTO_DIR,
     dataset_type=fo.types.ImageDirectory,
     name=DATASET_NAME,
-    overwrite=True
+    overwrite=True,
+    persistent=True
 )
 
+
+# By default it uses mobilenet-v2-imagenet-torch" 
+# https://docs.voxel51.com/model_zoo/models.html#mobilenet-v2-imagenet-torch
+# Trained on imagenet
 fob.compute_visualization(dataset, embeddings="default_embed", brain_key="default_embed")
 
+# Trained on 400 Million image text pairs from the internet
 model = foz.load_zoo_model("open-clip-torch")
 
 fob.compute_visualization(dataset, model=model, embeddings="open_clip_embed",
                               brain_key="openclip_embed")
 
-model2 = foz.load_zoo_model("mnasnet0.5-imagenet-torch")
+# Based on a DETR model trainied on 
+model2 = foz.load_zoo_model("detection-transformer-torch")
+fob.compute_visualization(dataset, model=model2, embeddings="det_transformer", brain_key="det_ransformer")
 
-fob.compute_visualization(dataset, model=model2, embeddings="mnasnet_embed",
-                              brain_key="mnasnet_embed")
+# trained on imagenet
+# model2 = foz.load_zoo_model("mnasnet0.5-imagenet-torch")
+
+# fob.compute_visualization(dataset, model=model2, embeddings="mnasnet_embed",
+                            #   brain_key="mnasnet_embed")
 
 # Calculate representativness
 # https://docs.voxel51.com/brain.html#image-representativeness
@@ -63,10 +74,3 @@ session.wait()
 print("done")
 
 
-# Now load an embedding model from the zoo. We are just going to use a well known simple one
-
-# Now run the embeddings
-
-# Talk about the "run" and show it with the dataset
-
-# Now visualize the embeddings in the app
--- 4_subsetting_expressions.py
+++ 4_subsetting_expressions.py
@@ -1,1 +1,35 @@
 import fiftyone as fo
+import fiftyone.brain as fob
+from fiftyone import ViewField as F
+
+"""
+We will look at multiple ways to sample and subset your data. This can be at the data import stage all the way through using expressions
+on calculated fields
+"""
+PHOTO_DIR = "/photos"
+
+# We could have done it at data import with random sampling
+random_import_subset = fo.Dataset.from_dir(
+    dataset_dir=PHOTO_DIR,
+    dataset_type=fo.types.ImageDirectory,
+    name="random_import_subset",
+    overwrite=True,
+    max_samples = 300,   
+    shuffle = True,
+    seed = 0.5
+
+)
+
+# We could also do it is a call on an existing dataset
+full_photos = fo.load_dataset("photos")
+
+# let's make a random clone so we don't mess with the original dataset
+random_clone_subset = full_photos.shuffle(seed=0.5).limit(300).clone("random_clone_subset", persistent = True)
+fob.compute_visualization(random_clone_subset, embeddings="open_clip_embed", brain_key="random_clip_embed")
+
+# Now let's actually subset using uniqueness and representativeness 
+unique_repr_clone_subset = full_photos.match((F("representativeness") > 0.5) & (F("uniqueness") > 0.5)).limit(300).clone("unique_repr_clone_subset", persistent = True)
+fob.compute_visualization(unique_repr_clone_subset, embeddings="open_clip_embed", brain_key="unique_clip_embed")
+
+# Now pick the rest of the photos for validation of our fine tune model
+final_validation = full_photos.exclude_by("filepath", unique_repr_clone_subset.values("filepath")).clone("validation_photos", persistent=True)
