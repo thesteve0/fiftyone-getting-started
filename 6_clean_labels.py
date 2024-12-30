84b7e136eb826232f02909070b52befee232d13f
Steven Pousty <steve.pousty@gmail.com>
2024-12-29 16:57:37 -0800

    Update 6_clean_labels.py

--- 6_clean_labels.py
+++ 6_clean_labels.py
@@ -1,6 +1,5 @@
 import fiftyone as fo
 
 """
-We are going to do a little data cleaning and prep but then load the data I did the whole process on. It would take too long in the workshop 
-to do all the data together.
+We are going to do a little data cleaning and prep but then load the data I finished cleaning. Given the length of the workshop, cleaning the labels would take too long to do together.
 """
